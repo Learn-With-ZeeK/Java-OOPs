00cc1c621863dce9c1578a8c56c203e600901440
Zeeshan A Khan <104137451+Learn-With-ZeeK@users.noreply.github.com>
2022-05-18 12:33:51 +0530

    main method

--- src/main/java/com/oops/Main.java
+++ src/main/java/com/oops/Main.java
@@ -2,7 +2,7 @@ package com.oops;
 
 public class Main {
 
-	public static void main(String[] args) {
+	 public static void main(String[] args) {
 		Deskphone myPhone= new Deskphone("7017756457");
 		myPhone.powerOn();
 		myPhone.callPhone("7017756457");
